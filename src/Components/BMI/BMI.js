451fd48f44ad21725998347e504ebc2b985f2ac8
Apu-Emdad <apuemdad96@gmail.com>
2021-10-19 17:53:42 +0600

    second commit

---
+++ src/Components/BMI/BMI.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BMI = () => {
+    return (
+        <div>
+            <h5>This is bmi</h5>
+        </div>
+    );
+};
+
+export default BMI;
---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAqrdkqzYB9ASaCOcgvWGbrnq9EdOdtCQ4",
+  authDomain: "curepoint-clinic.firebaseapp.com",
+  projectId: "curepoint-clinic",
+  storageBucket: "curepoint-clinic.appspot.com",
+  messagingSenderId: "453091804034",
+  appId: "1:453091804034:web:cb8d8ca21b310c6e8c459b",
+};
+
+export default firebaseConfig;
